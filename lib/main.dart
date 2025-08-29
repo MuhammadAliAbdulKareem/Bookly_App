45af8b61f45c997615f2d1af6a6757842386dd73
MuhammadAliAbdulKareem <muhammad.ali.abdulkareem@gmail.com>
2025-08-29 17:27:38 +0300

    Add MultIBlocProviders & Create The ServiceLocatorSetup Function

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/features/home/data/models/repos/home_repo_impl.dart';
+import 'package:bookly/features/home/presentation/view_model(manager)/featured_books_cubit/featured_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'constants.dart';
+import 'core/utils/service_locator.dart';
+import 'features/home/presentation/view_model(manager)/newest_books_cubit/newest_books_cubit.dart';
 
 void main() {
+  serviceLocatorSetup();
   runApp(const Bookly());
 }
 
@@ -18,17 +24,26 @@ class Bookly extends StatelessWidget {
       designSize: const Size(375, 812),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (context, child) => MaterialApp.router(
-        routerConfig: AppRouter.router,
-        debugShowCheckedModeBanner: false,
-        title: 'Bookly',
-        theme: ThemeData.dark().copyWith(
-          scaffoldBackgroundColor: Constants.kPrimaryColor,
-          textTheme: GoogleFonts.montserratTextTheme(
-            ThemeData.dark().textTheme,
+      builder: (context, child) => MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => FeaturedBooksCubit(getIt.get<HomeRepoImpl>()),
           ),
+          BlocProvider(
+              create: (context) => NewestBooksCubit(getIt.get<HomeRepoImpl>())),
+        ],
+        child: MaterialApp.router(
+          routerConfig: AppRouter.router,
+          debugShowCheckedModeBanner: false,
+          title: 'Bookly',
+          theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: Constants.kPrimaryColor,
+            textTheme: GoogleFonts.montserratTextTheme(
+              ThemeData.dark().textTheme,
+            ),
+          ),
+          themeMode: ThemeMode.system,
         ),
-        themeMode: ThemeMode.system,
       ),
       // child: SplashView(),
     );
