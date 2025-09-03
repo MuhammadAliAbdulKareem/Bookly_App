e587e629028cabbf11c90f15fd9395e0dec1b8e0
MuhammadAliAbdulKareem <muhammad.ali.abdulkareem@gmail.com>
2025-09-04 02:14:37 +0300

    Create Latest Version Of The Skeletonizer Widget In The App

--- lib/features/home/presentation/views/widgets/featured_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/featured_books_list_view.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
+import '../../../../../core/utils/assets_manager.dart';
 import 'custom_book_item.dart';
 
 class FeaturedBooksListView extends StatelessWidget {
@@ -37,30 +38,32 @@ class FeaturedBooksListView extends StatelessWidget {
         } else if (state is FeaturedBooksFailure) {
           return CustomErrorWidget(errMessage: state.errMessage);
         } else {
+          // Loading state with Skeletonizer
           return SizedBox(
             height: 200.h,
-            child: SkeletonizerConfig(
-              data: const SkeletonizerConfigData(
-                effect: ShimmerEffect(
-                  duration: Duration(milliseconds: 1000),
-                  highlightColor: Colors.grey,
-                  baseColor: Colors.black12,
-                ),
+            child: Skeletonizer(
+              enabled: true,
+              effect: const ShimmerEffect(
+                duration: Duration(milliseconds: 1500),
+                baseColor: Color(0xFF2A2A2A), // Dark gray base
+                highlightColor: Color(0xFF404040),
               ),
-              child: Skeletonizer(
-                enabled: true,
-                child: ListView.builder(
-                  itemCount: 6,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) => Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8.w),
-                    child: Container(
+              child: ListView.builder(
+                padding: EdgeInsets.zero,
+                physics: const BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                itemCount: 6,
+                itemBuilder: (context, index) => Padding(
+                  padding: REdgeInsets.only(
+                    right: 16.0,
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(12.0.r),
+                    child: Image.asset(
+                      AssetsManager.testImage,
                       width: 150.w,
                       height: 200.h,
-                      decoration: BoxDecoration(
-                        // color: Colors.grey[700],
-                        borderRadius: BorderRadius.circular(12.r),
-                      ),
+                      fit: BoxFit.fill,
                     ),
                   ),
                 ),
--- lib/features/home/presentation/views/widgets/skeleton_book_item.dart
+++ lib/features/home/presentation/views/widgets/skeleton_book_item.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/assets_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:skeletonizer/skeletonizer.dart';
@@ -9,75 +10,51 @@ class SkeletonBookItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: REdgeInsets.symmetric(vertical: 7.0.h, horizontal: 30.0.w),
-      child: SkeletonizerConfig(
-        data: SkeletonizerConfigData(
-          effect: ShimmerEffect(
-            duration: const Duration(milliseconds: 1000),
-            highlightColor: Colors.grey[500]!,
-            baseColor: Colors.grey[700]!,
-          ),
+      child: Skeletonizer(
+        enabled: true,
+        effect: const ShimmerEffect(
+          duration: Duration(milliseconds: 1500),
+          baseColor: Color(0xFF2A2A2A), // Dark gray base
+          highlightColor: Color(0xFF404040),
         ),
-        child: Skeletonizer(
-          enabled: true,
-          child: Row(
-            children: [
-              Container(
+        child: Row(
+          children: [
+            // Use ClipRRect + Image.asset for shimmer effect
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12.0.r),
+              child: Image.asset(
+                AssetsManager.testImage,
                 width: 85.w,
                 height: 120.h,
-                decoration: BoxDecoration(
-                  color: Colors.grey[700],
-                  borderRadius: BorderRadius.circular(12.0.r),
-                ),
+                fit: BoxFit.fill,
               ),
-              SizedBox(width: 10.0.w),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      height: 20.h,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12.0.r),
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: 8.h),
-                    Container(
-                      width: 120.w,
-                      height: 16.h,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12.0.r),
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: 8.h),
-                    Row(
-                      children: [
-                        Container(
-                          width: 50.w,
-                          height: 16.h,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(12.0.r),
-                            color: Colors.white,
-                          ),
-                        ),
-                        SizedBox(width: 40.0.w),
-                        Container(
-                          width: 70.w,
-                          height: 16.h,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(12.0.r),
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+            ),
+            SizedBox(width: 10.0.w),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Sample Book Title Text',
+                    style: TextStyle(fontSize: 16.sp),
+                  ),
+                  SizedBox(height: 8.h),
+                  Text(
+                    'Author Name',
+                    style: TextStyle(fontSize: 14.sp),
+                  ),
+                  SizedBox(height: 8.h),
+                  Row(
+                    children: [
+                      Text('Free', style: TextStyle(fontSize: 12.sp)),
+                      SizedBox(width: 40.0.w),
+                      Text('4.8★', style: TextStyle(fontSize: 12.sp)),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
