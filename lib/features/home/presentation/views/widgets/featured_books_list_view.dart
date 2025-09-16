6e6f3ffbcb406b0d1a6ed7b3aea1888bb49a9189
MuhammadAliAbdulKareem <muhammad.ali.abdulkareem@gmail.com>
2025-09-17 02:02:20 +0300

    Solve Some Pagination Issues

--- lib/features/home/presentation/views/widgets/featured_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/featured_books_list_view.dart
@@ -1,28 +1,72 @@
 import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../view_model_manager/featured_books_cubit/featured_books_cubit.dart';
 import 'custom_book_item.dart';
 
-class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key, required this.books});
+class FeaturedBooksListView extends StatefulWidget {
+  const FeaturedBooksListView({
+    super.key,
+    required this.books,
+  });
   final List<BookEntity> books;
+
+  @override
+  State<FeaturedBooksListView> createState() => _FeaturedBooksListViewState();
+}
+
+class _FeaturedBooksListViewState extends State<FeaturedBooksListView> {
+  late final ScrollController _scrollController;
+  var currentPage = 0;
+  bool isLoading = false;
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(_onScroll);
+  }
+
+  void _onScroll() async {
+    if (_scrollController.hasClients) {
+      final maxScroll = _scrollController.position.maxScrollExtent;
+      final currentScroll = _scrollController.position.pixels;
+      if (maxScroll > 0 && currentScroll / maxScroll >= 0.7) {
+        if (!isLoading) {
+          isLoading = true;
+          await BlocProvider.of<FeaturedBooksCubit>(context)
+              .fetchFeaturedBooks(pageNumber: currentPage++);
+          isLoading = false;
+        }
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_onScroll);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 200.h,
       child: ListView.builder(
+        controller: _scrollController,
         padding: EdgeInsets.zero,
         physics: const BouncingScrollPhysics(),
         scrollDirection: Axis.horizontal,
-        itemCount: books.length,
+        itemCount: widget.books.length,
         itemBuilder: (context, index) => GestureDetector(
           onTap: () {
             GoRouter.of(context).push(
               AppRouter.kBookDetailsView,
-              extra: books[index],
+              extra: widget.books[index],
             );
           },
           child: Padding(
@@ -31,7 +75,7 @@ class FeaturedBooksListView extends StatelessWidget {
             ),
             child: CustomBookItem(
               width: 150,
-              imageUrl: books[index].imageUrl,
+              imageUrl: widget.books[index].imageUrl,
             ),
           ),
         ),
